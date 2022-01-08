8f579ba63e0ce411da072301242e92631cb33464
avagadro <j2cry.avagadro@gmail.com>
2022-01-08 16:03:30 +0300

    Main functionality

--- odometer.py
+++ odometer.py
@@ -1,4 +1,8 @@
 import settings
+import os
+import random
+import datetime as dt
+from math import pi, sin
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
@@ -7,13 +11,36 @@ SERVICE_URL = settings.SERVICE_URL if settings.SERVICE_URL else '/'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = settings.SECRET_KEY
-socket_io = SocketIO(app)
+socket = SocketIO(app)
 
 
 @app.route(f'{SERVICE_URL}/')
 def index():
-    return render_template('index.html')
+    files = [f for f in os.listdir('static/resources/')]
+    print(files)
+    filename = 'resources/' + random.choice(files)
+    print(filename)
+    return render_template('index.html', filename=filename)
+
+
+@socket.on('calculate')
+def calculate(data: dict):
+    latitude = data.get('latitude', 0)      # in degrees!
+    birthday = data.get('birthday', None)
+    if not latitude or not birthday:
+        return 0
+    # calculation of travel time in seconds
+    travel_time = dt.datetime.now() - dt.datetime.fromisoformat(birthday)
+    # print(travel_time.total_seconds())
+
+    # calculation of linear velocity at latitude
+    latitude = pi / 2 - abs(latitude) / 180 * pi
+    radius = settings.EARTH_RADIUS * sin(latitude)
+    velocity = 2 * pi * radius / 86400     # km/sec
+
+    mileage = travel_time.total_seconds() * (velocity + settings.EARTH_VELOCITY)
+    return mileage
 
 
 if __name__ == '__main__':
-    socket_io.run(app, host="0.0.0.0", port=settings.PORT)
+    socket.run(app, host="0.0.0.0", port=settings.PORT)
--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
 SERVICE_URL = '/spaceodometer'
 SECRET_KEY = 'f#V[v&awA+kwc*p:dH~D/~t(dh4({Y5='
 PORT = 8088
+
+EARTH_RADIUS = 6371     # km
+EARTH_VELOCITY = 29.765    # km/sec
